99dab30695e2838a35cbbcf64fbf135d4ee93cf9
Guangning Yu <hi@guangningyu.com>
2016-11-05 13:08:37 +0800

    added a config file

---
+++ config.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# weibo app name
+app_name = ''
+
+# weibo app config (required)
+app_key = ''
+app_secret = ''
+redirect_uri = ''
+
+# weibo account config (required)
+username = ''
+password = ''
+
+# keywords (required)
+keywords = '机器学习,大数据,深度学习,神经网络,云计算,区块链,数学模型,Hadoop,数理统计,人工智能,语音识别,图像识别,Python,数据科学,机器视觉'
